cdc5c2ea63745d17d239b8c203395470a32f2f67
Philipp Bogdanov <pbogdanov@geolife.co.uk>
2017-01-03 23:13:39 +0300

    add migration for Realm

--- app/src/main/java/com/example/philipp/logicgame/BaseApplication.java
+++ app/src/main/java/com/example/philipp/logicgame/BaseApplication.java
@@ -9,6 +9,12 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         Realm.init(this);
+        RealmConfiguration configuration = new RealmConfiguration.Builder()
+                .schemaVersion(BuildConfig.DATABASE_VERSION)
+                .migration(new RealmMigration())
+                .build();
+        Realm.getInstance(configuration);
     }
 }
---
+++ app/src/main/java/com/example/philipp/logicgame/RealmMigration.java
@@ -0,0 +1,10 @@
+package com.example.philipp.logicgame;
+
+import io.realm.DynamicRealm;
+
+public class RealmMigration implements io.realm.RealmMigration {
+    @Override
+    public void migrate(DynamicRealm realm, long oldVersion, long newVersion) {
+
+    }
+}
